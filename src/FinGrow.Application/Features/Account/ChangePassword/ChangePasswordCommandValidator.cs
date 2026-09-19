cd730ce7d653edb7bca1f09f1b613722258d402c
Tomas <maglianotomas@gmail.com>
2026-09-19 20:59:03 -0300

    feat(T-14): revocar todos los refresh tokens del usuario al cambiar la contrasenia

---
+++ src/FinGrow.Application/Features/Account/ChangePassword/ChangePasswordCommandValidator.cs
@@ -0,0 +1,22 @@
+﻿namespace FinGrow.Application.Features.Account.ChangePassword;
+
+using FluentValidation;
+
+public sealed class ChangePasswordCommandValidator : AbstractValidator<ChangePasswordCommand>
+{
+    public ChangePasswordCommandValidator()
+    {
+        RuleFor(command => command.CurrentPassword)
+            .NotEmpty()
+            .WithMessage("La contrasena actual es obligatoria.");
+
+        RuleFor(command => command.NewPassword)
+            .NotEmpty()
+            .MinimumLength(8)
+            .WithMessage("La nueva contrasena debe tener al menos 8 caracteres.");
+
+        RuleFor(command => command.NewPassword)
+            .NotEqual(command => command.CurrentPassword)
+            .WithMessage("La nueva contrasena debe ser distinta de la actual.");
+    }
+}
